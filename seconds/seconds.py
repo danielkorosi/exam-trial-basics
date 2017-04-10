08ad9b1b670099c5af4c1c86eb22259d38461f0f
Daniel Korosi <daniel.korosi42@gmail.com>
2017-04-10 14:23:18 +0200

    seconds exercise solved with slicing the list

--- seconds/seconds.py
+++ seconds/seconds.py
@@ -1,7 +1,8 @@
-def seconds(change_this):
-    pass
+def seconds(number_list):
     # Create a function that takes a list as a parameter,
     # and returns a new list with every second element from the orignal list
     # example: [1, 2, 3, 4, 5] should produce [2, 4]
-
+    number_list = number_list[1::2]
+    return number_list
+    
 print(seconds([1, 2, 3, 4, 5])) # should print [2, 4]
